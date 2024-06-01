b05e8734d4299ad992ff8089055c34045b8ec84f
Trevor Mumm <56982109+TrevorMumm@users.noreply.github.com>
2024-05-31 21:26:55 -0500

    f

--- leagues/forms.py
+++ leagues/forms.py
@@ -4,10 +4,10 @@ from .models import Team, Player
 class UploadFileForm(forms.Form):
     file = forms.FileField()
 
-class TeamForm(forms.ModelForm):
+class TeamCreationForm(forms.ModelForm):  # Make sure this is defined as TeamCreationForm
     class Meta:
         model = Team
-        fields = ['name']
+        fields = ['name', 'leagues']
 
 class AssignPlayerForm(forms.Form):
     player = forms.ModelChoiceField(queryset=Player.objects.all(), label="Select Player")
--- leagues/views.py
+++ leagues/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from .models import League, Team, Player, Auction
 from .utils import generate_bracket
-from .forms import TeamForm, UploadFileForm, AssignPlayerForm
+from .forms import TeamCreationForm, UploadFileForm, AssignPlayerForm
 from django import forms
 
 @login_required
